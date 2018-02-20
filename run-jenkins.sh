cb1935fef9c78da00e07984db286e88444853811
Al Dickson <alastair.dickson@gmail.com>
2018-02-20 21:30:43 +0000

    Initial commit for Jenkins repo

---
+++ run-jenkins.sh
@@ -0,0 +1,8 @@
+docker run \
+  --rm \
+  -u root \
+  -p 9999:8080 \
+  -p 50000:50000 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  jenkinsci/blueocean:latest
